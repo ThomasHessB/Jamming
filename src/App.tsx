e29bd80df5ee4a2e5f32a619b5713313289e57f5
ThomasHessB <130525918+ThomasHessB@users.noreply.github.com>
2023-08-24 12:43:05 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Spotify App</h1>
+      <p>I'm supposed to make a </p>
+    </div>
+  );
+}
